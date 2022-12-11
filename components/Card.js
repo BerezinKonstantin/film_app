4e2e9ad2852c72f4883927451f8a9fb14089da4c
KonstantinBerezin <berezinko90@gmail.com>
2022-12-11 14:47:32 +0500

    исправлен грид

--- components/Card.js
+++ components/Card.js
@@ -6,7 +6,13 @@ export default function Card({ film, year, cover, id, filmName }) {
     <Link className={styles.card__link} href={`/films/${id}`}>
       <div className={styles.card}>
         <div className={styles.card__img}>
-          <Image src={cover} alt="Постер фильма" fill></Image>
+          <Image
+            src={cover}
+            alt="Постер фильма"
+            width={222}
+            height={320}
+            priority
+          ></Image>
         </div>
         <p className={styles.card__film_name}>{filmName}</p>
         <p className={styles.card__film_year}>{year}</p>
